94cc3646c62df1e0362fc66e241d2fee14024ce2
jeppeter <jeppeter@gmail.com>
2017-11-23 17:00:51 +0800

    pass A055

--- test/tstparser.js
+++ test/tstparser.js
@@ -369,6 +369,47 @@ var debug_opthelp_set = function (keycls) {
 
 exports.debug_opthelp_set = debug_opthelp_set;
 
+var debug_2_jsonfunc = function (args, keycls, value) {
+    'use strict';
+    var setvalue;
+    var idx;
+    if (!Array.isArray(value)) {
+        throw new Error('not list type');
+    }
+    if ((value.length % 2) !== 0) {
+        throw new Error('not even sized');
+    }
+    setvalue = [];
+    idx = 0;
+    while (idx < value.length) {
+        setvalue.push(value[idx]);
+        idx += 2;
+    }
+    args[keycls.optdest] = setvalue;
+    return;
+};
+exports.debug_2_jsonfunc = debug_2_jsonfunc;
+
+var debug_upper_jsonfunc = function (args, keycls, value) {
+    'use strict';
+    var valid = false;
+    var setvalue;
+    if (typeof value === 'string' || value === null) {
+        valid = true;
+    }
+
+    if (!valid) {
+        throw new Error(util.format('[%s]not string type', value));
+    }
+    setvalue = null;
+    if (typeof value === 'string') {
+        setvalue = value.toUpperCase();
+    }
+    args[keycls.optdest] = setvalue;
+    return;
+};
+exports.debug_upper_jsonfunc = debug_upper_jsonfunc;
+
 
 test('A001', function (t) {
     'use strict';
@@ -1840,6 +1881,48 @@ test('A054', function (t) {
             t.equal(args.subcommand, 'dep', get_notice(t, 'subcommand dep'));
             t.deepEqual(args.dep_list, ["jsonval1", "jsonval2"], get_notice(t, 'dep_list [jsonval1,jsonval2]'));
             t.equal(args.dep_string, 'jsonstring', get_notice(t, 'dep_string jsonstring'));
+            t.deepEqual(args.subnargs, ['ww'], get_notice(t, 'subnargs [ww]'));
+            unlink_file_callback(depjsonfile, 'depjsonfile', t, function () {
+                unlink_file_callback(jsonfile, 'jsonfile', t, function () {
+                    t.end();
+                });
+            });
+        });
+    });
+});
+
+test('A055', function (t) {
+    'use strict';
+    var commandline = `        {            "verbose|v" : "+",            "$port|p" : {                "value" : 3000,                "type" : "int",                "nargs" : 1 ,                 "helpinfo" : "port to connect"            },            "dep" : {                "list|l!jsonfunc=debug_2_jsonfunc!" : [],                "string|s!jsonfunc=debug_upper_jsonfunc!" : "s_var",                "$" : "+"            }        }`;
+    var options;
+    var optionstr = `        {            "jsonlong" : "jsonfile"        }`;
+    var parser;
+    var args;
+    var depstrval;
+    var depliststr;
+
+    setup_before(t);
+    write_file_callback('parseXXXXXX.json', '{"dep":{"list" : ["jsonval1","jsonval2"],"string" : "jsonstring"},"port":6000,"verbose":3}\n', t, 'jsonfile', function (jsonfile) {
+        write_file_callback('parseXXXXXX.json', '{"list":["depjson1","depjson2"]}\n', t, 'depjsonfile', function (depjsonfile) {
+            depstrval = 'newval';
+            depliststr = '["depenv1","depenv2"]';
+            renew_variable('EXTARGSPARSE_JSONFILE', jsonfile);
+            renew_variable('DEP_JSONFILE', depjsonfile);
+            options = extargsparse.ExtArgsOption(optionstr);
+            options.priority = [extargsparse.ENV_COMMAND_JSON_SET, extargsparse.ENVIRONMENT_SET, extargsparse.ENV_SUB_COMMAND_JSON_SET];
+            parser = extargsparse.ExtArgsParse(options);
+            parser.load_command_line_string(commandline);
+            renew_variable('DEP_STRING', depstrval);
+            renew_variable('DEP_LIST', depliststr);
+
+
+
+            args = parser.parse_command_line(['--jsonfile', jsonfile, 'dep', 'ww']);
+            t.equal(args.verbose, 3, get_notice(t, 'verbose 3'));
+            t.equal(args.port, 6000, get_notice(t, 'port 6000'));
+            t.equal(args.subcommand, 'dep', get_notice(t, 'subcommand dep'));
+            t.deepEqual(args.dep_list, ["jsonval1"], get_notice(t, 'dep_list [jsonval1]'));
+            t.equal(args.dep_string, 'JSONSTRING', get_notice(t, 'dep_string JSONSTRING'));
             t.deepEqual(args.subnargs, ['ww'], get_notice(t, 'subnargs [ww]'));
             unlink_file_callback(depjsonfile, 'depjsonfile', t, function () {
                 unlink_file_callback(jsonfile, 'jsonfile', t, function () {
